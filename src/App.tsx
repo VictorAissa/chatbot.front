3efc67b063ff923855def25b4b55e0ba99a3ac3d
victor <prkone@hotmail.fr>
2025-03-17 23:12:28 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div>
+            <p className="text-4xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                provident, voluptatibus ipsum ea quisquam accusamus impedit
+                sapiente commodi obcaecati veritatis incidunt. Blanditiis,
+                dolorum reprehenderit voluptatibus sequi inventore obcaecati
+                temporibus ex.
+            </p>
+        </div>
+    );
+}
+
+export default App;
